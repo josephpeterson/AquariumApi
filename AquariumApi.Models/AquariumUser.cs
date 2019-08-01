6dc0f45936b0ac7c9b6ed0ef50d2a7b650fd06bd
Joseph Peterson <josephpetersonn@gmail.com>
2019-08-01 13:41:50 -0500

    Update AquariumUser.cs

--- AquariumApi.Models/AquariumUser.cs
+++ AquariumApi.Models/AquariumUser.cs
@@ -14,6 +14,7 @@ namespace AquariumApi.Models
         public string Username { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+        [Column(TypeName = "datetime2")]
         public DateTime SeniorityDate { get; set; }
         public string Role { get; set; }
 
