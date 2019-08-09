752007d8f8ab22c235c203a85892f482fb07de4d
Joseph Peterson <josephpetersonn@gmail.com>
2019-08-09 00:27:11 -0500

    Update AquariumDetail model to include fish species

--- AquariumApi.DataAccess/AquariumDao.cs
+++ AquariumApi.DataAccess/AquariumDao.cs
@@ -100,7 +100,7 @@ namespace AquariumApi.DataAccess
         {
             var aquarium = _dbAquariumContext.TblAquarium.AsNoTracking()
                 .Where(aq => aq.Id == aquariumId)
-                .Include(aq => aq.Fish)
+                .Include(aq => aq.Fish).ThenInclude(d => d.Species)
                 .Include(aq => aq.Feedings)
                 .Include(aq => aq.Device).ThenInclude(d => d.CameraConfiguration)
                 .First();
