3c3f197e66db05aa2d2265ebe69c3b1e62cdd24f
Joseph Peterson <josephpetersonn@gmail.com>
2019-10-16 11:26:41 -0500

    Update PostBoard.cs
    Add description field

--- AquariumApi.Models/PostBoard.cs
+++ AquariumApi.Models/PostBoard.cs
@@ -13,6 +13,7 @@ namespace AquariumApi.Models
         public int AuthorId { get; set; }
         public int CategoryId { get; set; }
         public string Title { get; set; }
+        public string Description { get; set; }
         public string Type { get; set; }
         public PrivacyTypes Privacy { get; set; }
         public DateTime Timestamp { get; set; }
